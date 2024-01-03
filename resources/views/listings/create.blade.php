b945ae48f717516c593c7ddaa67c69c964444a68
alnasimul <alnasimul@gmail.com>
2024-01-04 01:48:32 +0600

    crud functionality fully added

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -9,7 +9,7 @@ class="p-10 max-w-lg mx-auto mt-24"
     <p class="mb-4">Post a gig to find a developer</p>
 </header>
 
-<form method="POST" action="/listings">
+<form method="POST" action="/listings" enctype="multipart/form-data">
     @csrf
     <div class="mb-6">
         <label
@@ -21,6 +21,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="company"
+            value="{{ old('company') }}"
         />
         @error('company')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -36,6 +37,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="title"
             placeholder="Example: Senior Laravel Developer"
+            value="{{ old('title') }}"
         />
         @error('title')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -53,6 +55,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="location"
             placeholder="Example: Remote, Boston MA, etc"
+            value="{{ old('location') }}"
         />
         @error('location')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -67,6 +70,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="email"
+            value="{{ old('email') }}"
         />
         @error('email')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -84,6 +88,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="website"
+            value="{{ old('website') }}"
         />
         @error('website')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -99,13 +104,14 @@ class="p-10 max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="tags"
             placeholder="Example: Laravel, Backend, Postgres, etc"
+            value="{{ old('tags') }}"
         />
         @error('tags')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
         @enderror
     </div>
 
-    {{-- <div class="mb-6">
+    <div class="mb-6">
         <label for="logo" class="inline-block text-lg mb-2">
             Company Logo
         </label>
@@ -114,7 +120,10 @@ class="p-10 max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="logo"
         />
-    </div> --}}
+        @error('logo')
+        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+        @enderror
+    </div>
 
     <div class="mb-6">
         <label
@@ -128,6 +137,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             name="description"
             rows="10"
             placeholder="Include tasks, requirements, salary, etc"
+            value="{{ old('description') }}"
         ></textarea>
         @error('description')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,18 @@ Route::get('/', [ListingController::class, 'index']);
 // Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 // ---------------------------
 
 // Single Listing
@@ -53,9 +65,6 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // });
 
 // ---------------------------
-
-// Store Listing Data
-Route::post('/listings', [ListingController::class, 'store']);
 
 // we can find single listing in another way 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
